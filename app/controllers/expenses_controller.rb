118a02c5438f5f0e0f6e919fadb3038851b74fe6
TripleThreads <segnih8@gmail.com>
2019-12-19 00:19:23 +0300

    initial commit

---
+++ app/controllers/expenses_controller.rb
@@ -0,0 +1,44 @@
+class ExpensesController < ApplicationController
+  def index
+    @expenses = Expense.all
+  end
+
+  def show
+    @expense = Expense.find(params[:id])
+  end
+
+  def new
+    @expense = Expense.new
+  end
+
+  def create
+    @expense = Expense.new(expense_params)
+    if @expense.save
+      redirect_to @expense
+    else
+      render 'new'
+    end
+  end
+  def edit
+    @expense = Expense.find(params[:id])
+  end
+
+  def update
+    @expense = Expense.find(params[:id])
+    if @expense.update(expense_params)
+      redirect_to @expense
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @expense = Expense.find(params[:id])
+    @expense.destroy
+    redirect_to expense_path
+  end
+
+  private def expense_params
+    params.require(:expense).permit(:title, :amount, :body)
+  end
+end
---
+++ app/models/expense.rb
@@ -0,0 +1,3 @@
+class Expense < ApplicationRecord
+  validates :title, presence: true, length: {minimum: 5}
+end
